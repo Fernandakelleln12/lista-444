ce75d68eee32ad787ea825ff018f196c5f36c5cd
CJ3016595 <CJ3016595@LAB01-PC31>
2023-04-12 10:50:01 -0300

    Adicionar arquivos de projeto.

---
+++ lista 444/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_444
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //exercicio 4
+           //int num, num2, num3;
+        //    Console.WriteLine("Entre com um número: ");
+        //    num = int.Parse(Console.ReadLine());
+        //    num2 = num / 2;
+        //    num3 = num % 2;
+        //    if(num3 == 0 && num2 * 2 > 10)
+        //   Console.WriteLine("Mensagem 1");
+        //   else
+        //    Console.WriteLine("Mensagem 2");
+        //    Console.ReadKey();
+
+
+            //exercicio 5
+            int vel1, vel2, vel3;
+            Console.WriteLine("digite a velocidade de instante");
+            vel1=int.Parse(Console.ReadLine()); 
+            Console.WriteLine("digite a velocidade de inicial");
+            vel2=int.Parse(Console.ReadLine()); 
+            Console.WriteLine("digite a aceleração");
+            vel3=int.Parse(Console.ReadLine());
+
+            vel1 = vel2 + vel3;
+
+
+
+        //}
+    }
+}
